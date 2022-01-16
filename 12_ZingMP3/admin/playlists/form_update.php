7aae1ac45d3f24418506eaa3423cf34a4cdd853b
Manh <nvmanh25101@gmail.com>
2022-01-16 16:19:23 +0700

    update(admin): permission

--- 12_ZingMP3/admin/playlists/form_update.php
+++ 12_ZingMP3/admin/playlists/form_update.php
@@ -6,8 +6,15 @@
     if(empty($_GET['id'])) {
         $_SESSION['error'] = 'Không có dữ liệu để sửa!';
         header('location:index.php');
+        exit();
     }
 
+    if($_GET['admin_id'] !== $_SESSION['id'] || $_SESSION['level'] !== 1) {
+        $_SESSION['error'] = 'Bạn không có quyền để truy cập';
+        header('location:index.php');
+        exit();
+    }
+    
     $id = $_GET['id'];
     require_once '../../database/connect.php';
 
